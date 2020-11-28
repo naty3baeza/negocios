d7b859fcfce9dcd9cd51396881b8212b285f2c3e
= <=>
2020-11-28 18:28:19 -0300

    Creación de registro para almacenamiento de datos de cada MEF

--- negociacion_mef.erl
+++ negociacion_mef.erl
@@ -11,6 +11,13 @@ terminate/3, code_change/4,
 ocupada/2, ocupada/3, ocupada_espera/2, ocupada_espera/3, negociar/2,
 negociar/3, espera/2, lista/2, lista/3]).
 
+-record(state, {nombre="",
+                otro,
+                itemspropios=[],
+                otrositems=[],
+                monitor,
+                desde}).
+
 %%% API PUBLICA
 start(Name) ->
 gen_fsm:start(?MODULE, [Name], []).
